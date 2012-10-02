402b0a207f18bbefb8ffa09dfd04cde5124356ee
Patrick Amihood <pamihood@gmail.com>
2012-10-02 13:56:36 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FavyApp::Application.config.secret_token = '313eeecadbd16955340b7c2fe3c1362bec63f72b5baa59f3bc812c416726bf2b40a05cb5a31c26c11dd9bddb13494dbc0831f8706cc515ed35650d84c4813966'
